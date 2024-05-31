74d781722c70a3d6eae22931811e254be9d39d14
sarow <msarowar947@gmail.com>
2024-05-31 16:40:31 +0600

    new code here

---
+++ bin/if-else.dart
@@ -0,0 +1,27 @@
+void main(){
+
+  int age = 21;
+// jodi karo age 21 er beshi hoi, then he is "man"
+// if ( condition ){
+// body
+// }
+if(age >= 21){
+  print('man');
+} else if (age >= 11) {
+  print('Boy');
+} else {
+  print('Baby boy');
+}
+
+String userName = 'Shamim';
+
+if (userName == 'sunny' && age > 20){
+  print('UserName is $userName');
+}
+
+if (userName == 'Shamim' || userName == 'sunny'){
+  print('welcome home');
+}
+
+
+}
