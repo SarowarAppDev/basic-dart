bee5f83f0b24330d138dfcb3f144e9176263da04
sarow <msarowar947@gmail.com>
2024-05-31 09:30:48 +0600

    updated code

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,10 +1,18 @@
 void main() {
   // single line comment ;
   print('hello world');
+  print('Good day');
+  print(244);
 
-
-
-
+  /*
+  multiline comment;
+  multiline comment;
+  multiline comment;
+   */
+  print(24+23);
+  print(10-20);
+  print(10*10);
+  print(10/50);
 
 }
 
