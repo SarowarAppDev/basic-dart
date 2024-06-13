6d0afd6f74c4eafa006da422799adff638dd2c2b
sarow <msarowar947@gmail.com>
2024-06-13 13:09:54 +0600

    new code

--- bin/Code_Practice.dart
+++ bin/Code_Practice.dart
@@ -1,27 +1,4 @@
 void main(){
 
-  // ?, ??, !
-
-  late String university;
-  int? myAge = null; // nullable int
-  myAge = 30;
-  // null
-  print(myAge);
-
-  String? myName;
-
-  if (myName == null) {
-    print('Hello');
-  }else {
-    print(myName);
-  }
-
-  print(myName ?? 'Hello');
-  print(myName!);
-
-  myName = 'Hasib';
-  print(myName);
-
-
 
 }
