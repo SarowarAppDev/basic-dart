2c3b7fa690564da713e7d018742145962a6e9cc1
sarow <msarowar947@gmail.com>
2024-06-13 21:33:39 +0600

    code push

---
+++ bin/string_intepolation.dart
@@ -0,0 +1,18 @@
+void main(){
+  String name = 'Sarowar';
+  int studentId =212504;
+  String cloogle = 'B.P.I';
+  double cgpa = 3.62;
+
+  String myProfile = 'hello my name is:$name and cloogle name:$cloogle and my roll:$studentId and my diploma cgpa:$cgpa';
+  print(myProfile);
+
+  int num1 = 35;
+  int num2 = 345;
+  int  num3 = num1 + num2;
+  String plus = 'the num1 and num2 result is $num3 ' + '${354+35+54+543}';
+  print(plus);
+
+  String myName = ' Md.Sarowar';
+  print('Hello World I am $myName');
+}
